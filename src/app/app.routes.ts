6d441e68e26a06df64eb2f58152725773d423225
Maamoun Okla <mamoun95bo@icloud.com>
2024-05-17 19:57:40 +0200

    init repo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "home" , component: HomeComponent},
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    
+    { path: '404', component: NotFoundComponent },
+    { path: '**', component: NotFoundComponent }
+];
+
